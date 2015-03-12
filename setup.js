4ead66cd675986046bf87d0bd7f9dbee831e141a
Neal Conner <neal.conner@lamassu.is>
2015-03-12 15:35:39 -0400

    Create setup.js

---
+++ setup.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var LamassuConfig = require('lamassu-config');
+var promptly = require('promptly');
+
+var config = new LamassuConfig();
+
+console.log('\nSetting up the Coinapult wallet and trading plugin.\n');
+console.log('Please enter your API credentials.\n');
+promptly.prompt('API Key: ', function(keyErr, key) {
+  promptly.password('API Secret: ', function(secretErr, secret) {
+    promptly.prompt('Currency (three letter ISO code, same as machine): ', function(fiatCurrencyErr, fiatCurrency) {
+      updateDb(key, secret, fiatCurrency, function(err) {
+        if (err) throw err;
+        console.log('\nSuccess.');
+      });
+    });
+  });
+});
+
+function updateDb(key, secret, fiatCurrency, callback) {
+  var newConfig = {
+    exchanges: {
+      plugins: {
+        settings: {
+          coinapult: {
+            guid: key,
+            password: secret,
+            fromAddress: '',
+            key: key,
+            clientId: '',
+            secret: secret,
+            fiatCurrency: fiatCurrency
+          }
+        },
+        current: {
+          ticker: 'coinapult',
+          transfer: 'coinapult',
+          wallet: 'coinapult',
+          trade: 'coinapult'
+        }
+      }
+    }
+  };
+  config.mergeConfig(newConfig, callback);
+}
